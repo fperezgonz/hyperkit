b2fd910c90f9bf518108d0eb831f7da320895d36
francisco.perez <fperezgonz@gmail.com>
2023-05-31 19:29:27 +0200

    Collect property data

--- lib/src/main/java/solutions/sulfura/gend/DtoAnnotationProcessor.java
+++ lib/src/main/java/solutions/sulfura/gend/DtoAnnotationProcessor.java
@@ -11,9 +11,11 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.ExecutableType;
 import javax.lang.model.type.MirroredTypesException;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
+import java.lang.reflect.Type;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -34,56 +36,116 @@ public class DtoAnnotationProcessor extends AbstractProcessor {
 
         Set<TypeElement> elements = (Set<TypeElement>) roundEnv.getElementsAnnotatedWith(Dto.class);
 
-        TypeKind[] fieldKinds = {TypeKind.ARRAY,
-                TypeKind.BYTE,
-                TypeKind.CHAR,
-                TypeKind.BOOLEAN,
-                TypeKind.INT,
-                TypeKind.LONG,
-                TypeKind.FLOAT,
-                TypeKind.DOUBLE,
-                TypeKind.SHORT,
-                TypeKind.TYPEVAR,
-                TypeKind.DECLARED
-        };
-
         for (TypeElement element : elements) {
-
-            element.asType();
-            Dto dtoAnnotation = element.getAnnotation(Dto.class);
-            List<? extends TypeMirror> types = null;
-
-            try {
-                dtoAnnotation.include();
-            } catch (MirroredTypesException mte) {
-                types = mte.getTypeMirrors();
-            }
-
-            final List<? extends TypeMirror> finalIncludedTypes = types;
-
-            List<? extends Element> publicFields = element.getEnclosedElements().stream()
-                    .filter(enclosedElement ->
-                            enclosedElement.getKind() == ElementKind.FIELD
-                                    && Arrays.stream(fieldKinds).anyMatch(kind -> kind == enclosedElement.asType().getKind())
-                                    && enclosedElement.getModifiers().contains(Modifier.PUBLIC)
-                                    && (enclosedElement.getAnnotation(DtoProperty.class) != null || finalIncludedTypes.size() == 0 || finalIncludedTypes.stream()
-                                    .anyMatch(annotationType -> enclosedElement.getAnnotationMirrors().contains(annotationType)))
-                    )
-                    .collect(Collectors.toList());
-            List<? extends Element> gettersAndSetters = element.getEnclosedElements().stream()
-                    .filter(enclosedElement ->
-                            enclosedElement.getKind() == ElementKind.METHOD
-                                    && enclosedElement.getModifiers().contains(Modifier.PUBLIC)
-                                    && (enclosedElement.getAnnotation(DtoProperty.class) != null || finalIncludedTypes.size() == 0 || finalIncludedTypes.stream()
-                                    .anyMatch(annotationType -> enclosedElement.getAnnotationMirrors().contains(annotationType)))
-                                    && (enclosedElement.getSimpleName().toString().startsWith("get") || enclosedElement.getSimpleName().toString().startsWith("is") || enclosedElement.getSimpleName().toString().startsWith("set"))
-                    )
-                    .collect(Collectors.toList());
-
-            return true;
+            createDtoFromClassElement(element);
         }
 
         return true;
+
+    }
+
+    public void createDtoFromClassElement(TypeElement element) {
+
+        Dto dtoAnnotation = element.getAnnotation(Dto.class);
+        List<? extends TypeMirror> types = null;
+
+        try {
+            dtoAnnotation.include();
+        } catch (MirroredTypesException mte) {
+            types = mte.getTypeMirrors();
+        }
+
+        final List<? extends TypeMirror> finalIncludedTypes = types;
+
+        Map<String, DtoPropertyData> dtoProperties = new HashMap<>();
+
+        //Collect public field data
+        List<? extends Element> publicFields = element.getEnclosedElements().stream()
+                .filter(enclosedElement ->
+                        enclosedElement.getKind() == ElementKind.FIELD
+                                && enclosedElement.getModifiers().contains(Modifier.PUBLIC)
+                                && (enclosedElement.getAnnotation(DtoProperty.class) != null || finalIncludedTypes.isEmpty() || finalIncludedTypes.stream()
+                                .anyMatch(annotationType -> enclosedElement.getAnnotationMirrors().contains(annotationType)))
+                )
+                .collect(Collectors.toList());
+
+        for (Element field : publicFields) {
+            String fieldName = field.getSimpleName().toString();
+            dtoProperties.put(fieldName, new DtoPropertyData(fieldName, true, true));
+        }
+
+        //Collect getter and setter data
+        List<? extends Element> gettersAndSetters = element.getEnclosedElements().stream()
+                .filter(enclosedElement ->
+                        enclosedElement.getKind() == ElementKind.METHOD
+                                && enclosedElement.getModifiers().contains(Modifier.PUBLIC)
+                                && (enclosedElement.getAnnotation(DtoProperty.class) != null || finalIncludedTypes.isEmpty() || finalIncludedTypes.stream()
+                                .anyMatch(annotationType -> enclosedElement.getAnnotationMirrors().contains(annotationType)))
+                                &&
+                                (
+                                        ((enclosedElement.getSimpleName().toString().startsWith("get")
+                                                || enclosedElement.getSimpleName().toString().startsWith("is"))
+                                                && ((ExecutableType) enclosedElement.asType()).getReturnType().getKind() != TypeKind.VOID
+                                                && ((ExecutableType) enclosedElement.asType()).getParameterTypes().isEmpty()
+                                        )
+                                                || (enclosedElement.getSimpleName().toString().startsWith("set")
+                                                && ((ExecutableType) enclosedElement.asType()).getReturnType().getKind() == TypeKind.VOID
+                                                && ((ExecutableType) enclosedElement.asType()).getParameterTypes().size() == 1
+                                        )
+                                )
+                )
+                .collect(Collectors.toList());
+
+        for (Element getterSetter : gettersAndSetters) {
+
+            String getterSetterName = getterSetter.getSimpleName().toString();
+
+            boolean canRead = false;
+            boolean canWrite = false;
+
+            if (getterSetterName.startsWith("get")) {
+                canRead = true;
+                getterSetterName = getterSetterName.substring(0, 3);
+            } else if (getterSetterName.startsWith("is")) {
+                canRead = true;
+                getterSetterName = getterSetterName.substring(0, 2);
+            } else if (getterSetterName.startsWith("set")) {
+                canWrite = true;
+                getterSetterName = getterSetterName.substring(0, 3);
+            }
+
+            DtoPropertyData propertyData = dtoProperties.get(getterSetterName);
+
+            if (propertyData == null) {
+                propertyData = new DtoPropertyData(getterSetter.getSimpleName().toString(), false, false);
+                dtoProperties.put(getterSetterName, propertyData);
+            }
+
+            if (canRead) {
+                propertyData.canRead = true;
+            }
+
+            if (canWrite) {
+                propertyData.canWrite = true;
+            }
+
+        }
+
+        //Generate code
+        //TODO
+    }
+
+    public static class DtoPropertyData {
+
+        public DtoPropertyData(String name, boolean canRead, boolean canWrite) {
+            this.name = name;
+            this.canRead = canRead;
+            this.canWrite = canWrite;
+        }
+
+        String name;
+        boolean canRead;
+        boolean canWrite;
 
     }
 
