f9950795b949d29e975199ceb9e1d54330578cdd
francisco.perez <fperezgonz@gmail.com>
2023-07-02 22:01:58 +0200

    Fix on circular dependencies test source classes

--- lib/src/test/resources/circular_dependencies/SourceClassA.java
+++ lib/src/test/resources/circular_dependencies/SourceClassA.java
@@ -1,6 +1,7 @@
 package solutions.sulfura.gend.dtos.circular_dependencies;
 
 import solutions.sulfura.gend.dtos.annotations.Dto;
+import java.util.List;
 
 /**This class is used as input for the DTO generator test */
 @Dto
