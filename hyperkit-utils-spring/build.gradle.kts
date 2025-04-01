d27bd99a42c7ac619980574328a87f271abbb5f3
fperezgonz <fperezgonz@gmail.com>
2025-04-02 00:52:14 +0200

    Fix hyperkit-utils-spring build and publication

--- hyperkit-utils-spring/build.gradle.kts
+++ hyperkit-utils-spring/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("org.springframework.boot") version "3.2.1"
-    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "solutions.sulfura"
@@ -45,10 +43,10 @@ dependencies {
     implementation(project(":hyperkit-projections-dsl"))
     implementation("org.jspecify:jspecify:1.0.0")
     implementation("org.hsqldb:hsqldb:2.7.1")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.4.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.4")
     implementation("io.github.perplexhub:rsql-jpa-spring-boot-starter:6.0.26")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- hyperkit-utils-spring/src/test/java/solutions/sulfura/hyperkit/utils/spring/SpringTestConfig.java
+++ hyperkit-utils-spring/src/test/java/solutions/sulfura/hyperkit/utils/spring/SpringTestConfig.java
@@ -1,6 +1,5 @@
 package solutions.sulfura.hyperkit.utils.spring;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import solutions.sulfura.hyperkit.utils.OptionValueWrapperAdapter;
@@ -12,10 +11,6 @@ public class SpringTestConfig {
     @Bean
     public ValueWrapperAdapter<?> valueWrapperAdapter() {
         return new OptionValueWrapperAdapter();
-    }
-
-    @Bean JavaTimeModule javaTimeModule() {
-        return new JavaTimeModule();
     }
 
 }
