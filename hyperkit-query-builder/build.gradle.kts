466890046e0c22e508f66f0b7ebace1d41b8db0f
fperezgonz <fperezgonz@gmail.com>
2025-10-26 00:44:15 +0200

    Add `test` and `check` tasks, update `build` and `publish` tasks in `hyperkit-query-builder`

--- hyperkit-query-builder/build.gradle.kts
+++ hyperkit-query-builder/build.gradle.kts
@@ -1,10 +1,29 @@
 import org.apache.tools.ant.taskdefs.condition.Os
 
-tasks.register<Exec>("build") {
+tasks.register<Exec>("test") {
     workingDir = project.projectDir
+    group = "verification"
 
     if (Os.isFamily(Os.FAMILY_WINDOWS)) {
-        commandLine("cmd", "npm", "run", "build")
+        commandLine("cmd", "/c", "npm", "run", "test")
+    } else {
+        commandLine("npm", "run", "test")
+    }
+
+}
+
+tasks.register<DefaultTask>("check") {
+    group = "verification"
+    dependsOn("test")
+}
+
+tasks.register<Exec>("build") {
+    workingDir = project.projectDir
+    group = "build"
+    dependsOn("check")
+
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        commandLine("cmd", "/c", "npm", "run", "build")
     } else {
         commandLine("npm", "run", "build")
     }
@@ -15,7 +34,7 @@ tasks.register<Exec>("publish") {
     workingDir = project.projectDir
 
     if (Os.isFamily(Os.FAMILY_WINDOWS)) {
-        commandLine("cmd", "npm", "publish")
+        commandLine("cmd", "/c", "npm", "publish")
     } else {
         commandLine("npm", "publish")
     }
