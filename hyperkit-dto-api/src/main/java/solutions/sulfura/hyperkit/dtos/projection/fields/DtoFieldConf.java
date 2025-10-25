98188dbac41444efbc5b4c4e43ef467e080888a9
fperezgonz <fperezgonz@gmail.com>
2025-10-26 00:44:33 +0200

    Fix typos in FieldConf and DtoFieldConf class comments

--- hyperkit-dto-api/src/main/java/solutions/sulfura/hyperkit/dtos/projection/fields/DtoFieldConf.java
+++ hyperkit-dto-api/src/main/java/solutions/sulfura/hyperkit/dtos/projection/fields/DtoFieldConf.java
@@ -5,8 +5,8 @@ import solutions.sulfura.hyperkit.dtos.projection.DtoProjection;
 import java.util.Objects;
 
 /**
- * The purpose of this class is to define presence or absence of the field on processes that adhere to this configuration, such as database queries or request data
- * ¿How should it handle permissions? If a field is declared as mandatory but the context has no permissions over it, then the process should fail. If a field is declared as IGNORED, the field should be set with NONE and if the field is set with another value, its value should be ignored by the processes using this config and treat it as NONE
+ * The purpose of this class is to define the presence or absence of the field on processes that adhere to this configuration, such as database queries or request data
+ * How should it handle permissions? If a field is declared as mandatory but the context has no permissions over it, then the process should fail. If a field is declared as IGNORED, the field should be set with NONE and if the field is set with another value, its value should be ignored by the processes using this config and treat it as NONE
  * If a field is not defined as MANDATORY or IGNORED its presence or absence should be determined by the context permissions
  */
 public class DtoFieldConf<T extends DtoProjection<?>> extends FieldConf {
--- hyperkit-dto-api/src/main/java/solutions/sulfura/hyperkit/dtos/projection/fields/FieldConf.java
+++ hyperkit-dto-api/src/main/java/solutions/sulfura/hyperkit/dtos/projection/fields/FieldConf.java
@@ -6,7 +6,7 @@ import java.util.Objects;
  * The purpose of this class is to define presence or absence of the field on processes that adhere to this configuration, such as database queries or request data
  * If a field is IGNORED, the field should be set with NONE and if the field is set with any other value, its value should be ignored by the processes using this config and treat it as NONE
  * Null values will be treated as IGNORED
- * ¿How should it handle permissions?
+ * How should it handle permissions?
  * - If a field is MANDATORY but the context has no permissions over it, then the process should fail.
  * - If a field is not MANDATORY or IGNORED its presence or absence should be determined by the context permissions
  */
