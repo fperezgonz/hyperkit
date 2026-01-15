1dbe32bab6ff2c116f780956998049fd793f96cb
fperezgonz <fperezgonz@gmail.com>
2026-01-15 14:49:54 +0100

    Add POM metadata for Maven publication

--- hyperkit-dto-api/build.gradle.kts
+++ hyperkit-dto-api/build.gradle.kts
@@ -9,12 +9,38 @@ repositories {
 }
 
 publishing {
+
     publications {
         create<MavenPublication>("maven") {
             artifactId = "hyperkit-dto-api"
             from(components["java"])
+
+            pom {
+                name = "HyperKit Dto API"
+                description = "The dto model api used by Hyperkit projects"
+                url = "https://gitlab.com/sulfura/hyperkit/-/tree/master/hyperkit-dto-api"
+                inceptionYear = "2023"
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "fperezgonz@gmail.com"
+                        name = "Francisco José Pérez Gonzalez"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://gitlab.com/sulfura/hyperkit.git"
+                    developerConnection = "scm:git:ssh://gitlab.com:sulfura/hyperkit.git"
+                    url = "https://gitlab.com/sulfura/hyperkit"
+                }
+            }
         }
     }
+
     repositories {
         maven {
             name = "Gitlab"
@@ -28,6 +54,7 @@ publishing {
             }
         }
     }
+
 }
 
 dependencies {
