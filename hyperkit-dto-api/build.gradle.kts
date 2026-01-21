2e5011e10c39208946232f4bfc1947e46ede8f16
fperezgonz <fperezgonz@gmail.com>
2026-01-21 11:28:52 +0100

    Configure deployments to Maven Central with JReleaser

--- hyperkit-dto-api/build.gradle.kts
+++ hyperkit-dto-api/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jreleaser.model.Active
+import kotlin.io.encoding.Base64
+
 plugins {
     `java-library`
     `maven-publish`
+    id("org.jreleaser") version "1.22.0"
 }
 
 repositories {
@@ -53,8 +57,80 @@ publishing {
                 create<HttpHeaderAuthentication>("header")
             }
         }
+        // Staging repository to prepare deployments to Maven Central
+        maven {
+            name = "MavenCentralStaging"
+            url = project.layout.buildDirectory.dir("staging-deploy-$version").get().asFile.toURI()
+        }
+    }
+}
+
+jreleaser {
+
+    project {
+        authors = listOf("Francisco José Pérez González")
+        license = "Apache-2.0"
+        links {
+            homepage = "https://gitlab.com/sulfura/hyperkit"
+        }
     }
 
+    // Prevent the "release.gitlab.token must not be blank" error
+    yolo = true
+
+//    release {
+//        enabled = false
+//        // Prevent the "release.gitlab.token must not be blank" error
+//        gitlab {
+//
+//            skipRelease = true
+//            token = "no-token"
+//        }
+//    }
+
+    signing {
+        active = Active.SNAPSHOT
+
+        val isSecretKeySet = System.getenv("MAVEN_SIGNING_SECRET_KEY_B64") != null
+        val isPublicKeySet = System.getenv("MAVEN_SIGNING_PUBLIC_KEY_B64") != null
+
+        if (!isSecretKeySet) {
+            logger.warn("MAVEN_SIGNING_SECRET_KEY_B64 environment variable is not set. Some JReleaser tasks will fail")
+            return@signing
+        }
+        if (!isPublicKeySet) {
+            logger.warn("MAVEN_SIGNING_PUBLIC_KEY_B64 environment variable is not set. Some JReleaser tasks will fail")
+            return@signing
+        }
+
+        pgp {
+            armored = true
+            secretKey = String(Base64.decode(System.getenv("MAVEN_SIGNING_SECRET_KEY_B64")))
+            publicKey = String(Base64.decode(System.getenv("MAVEN_SIGNING_PUBLIC_KEY_B64")))
+            passphrase = System.getenv("MAVEN_SIGNING_SECRET_KEY_PASSPHRASE")
+        }
+    }
+
+    deploy {
+        maven {
+            nexus2 {
+                create("snapshot-deploy") {
+                    url = "https://ossrh-staging-api.central.sonatype.com/service/local/"
+                    username = System.getenv("SONATYPE_TOKEN_USERNAME")
+                    password = System.getenv("SONATYPE_TOKEN_PASSWORD")
+                    gitRootSearch = true
+                    active = Active.SNAPSHOT
+                    snapshotUrl = "https://central.sonatype.com/repository/maven-snapshots/"
+                    applyMavenCentralRules = true
+                    snapshotSupported = true
+                    closeRepository = true
+                    releaseRepository = true
+
+                    stagingRepository("build/staging-deploy-$version")
+                }
+            }
+        }
+    }
 }
 
 dependencies {
