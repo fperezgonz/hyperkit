e70ec9964295206fdcd809f2984f9ef6f550942e
fperezgonz <fperezgonz@gmail.com>
2026-02-01 20:31:42 +0100

    Replace logger with System.out for printing debug information

--- hyperkit-dto-generator-maven-plugin/src/main/java/solutions/sulfura/GenerateDtosMojo.java
+++ hyperkit-dto-generator-maven-plugin/src/main/java/solutions/sulfura/GenerateDtosMojo.java
@@ -48,8 +48,8 @@ public class GenerateDtosMojo extends AbstractMojo {
                         return it.getFile().getAbsolutePath();})
                     .collect(Collectors.toList());
 
-        getLog().info("project.basedir=" + project.getBasedir());
-        getLog().info("rootOutputPath=" + rootOutputPath);
+        System.out.println("project.basedir=" + project.getBasedir());
+        System.out.println("rootOutputPath=" + rootOutputPath);
 
         var generator = new DtoGenerator(resolvedInputPaths,
                 classpathElements,
